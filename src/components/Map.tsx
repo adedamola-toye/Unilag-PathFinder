892b5aee4b8785086acddcdeafca05155c395885
adedamola-toye <toye.adedamola@gmail.com>
2024-09-25 08:37:14 +0100

    Still working on map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,50 +1,79 @@
-import {useEffect, useState} from 'react';
-import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import { useEffect, useState } from 'react';
+import { MapContainer, Marker, Popup, useMap } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
 import { getCoordinates } from './getCoordinates';
 import { locations } from './locations';
 
 interface Coordinate {
-    latitude : number;
+    latitude: number;
     longitude: number;
-    name: string
+    name: string;
 }
 
-function Map(){
-    const [locationCoordinates, setLocationCoordinates] = useState<Coordinate[]>([])
+const getRandomOffset = () => {
+    const offsetFactor = 0.0001; 
+    return Math.random() * offsetFactor - offsetFactor / 2;
+};
+
+function FitMapToBounds({ coordinates }: { coordinates: Coordinate[] }) {
+    const map = useMap();
 
     useEffect(() => {
-        const getAllCoordinates = async() => {
+        if (coordinates.length > 0) {
+            const bounds = coordinates.map(coord => [coord.latitude, coord.longitude] as [number, number]);
+            map.fitBounds(bounds);
+        }
+    }, [coordinates, map]);
+
+    return null;
+}
+
+function Map() {
+    const [locationCoordinates, setLocationCoordinates] = useState<Coordinate[]>([]);
+
+    useEffect(() => {
+        const getAllCoordinates = async () => {
             const coordinatesArray: Coordinate[] = [];
-            for(const location of locations){
+            for (const location of locations) {
                 const result = await getCoordinates(location);
-                if(result){
+                if (result) {
                     coordinatesArray.push({
                         latitude: parseFloat(result.latitude),
                         longitude: parseFloat(result.longitude),
-                        name: location,  
+                        name: location,
                     });
                 }
             }
-            setLocationCoordinates(coordinatesArray)
+            setLocationCoordinates(coordinatesArray);
         };
         getAllCoordinates();
     }, []);
 
-
     return (
-        <MapContainer center={[6.5244, 3.3792]} zoom={15} style={{ height: "500px", width: "100%" }}>
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
-          {locationCoordinates.map((location, index) => (
-            <Marker key={index} position={[location.latitude, location.longitude]}>
-              <Popup>{location.name}</Popup>
-            </Marker>
-          ))}
+        <MapContainer 
+            center={[6.5244, 3.3792]} 
+            zoom={19} 
+            minZoom={20}
+            style={{ height: "500px", width: "100%" }} 
+            scrollWheelZoom={false} 
+        >
+            
+            <FitMapToBounds coordinates={locationCoordinates} />
+
+           
+            {locationCoordinates.map((location, index) => (
+                <Marker 
+                    key={index} 
+                    position={[
+                        location.latitude + getRandomOffset(), 
+                        location.longitude + getRandomOffset()
+                    ]}
+                >
+                    <Popup>{location.name}</Popup>
+                </Marker>
+            ))}
         </MapContainer>
-      );
-    
+    );
 }
-export default Map
+
+export default Map;
