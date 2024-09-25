58fd4205686dbbaef20422e57aca9fc81d879202
adedamola-toye <toye.adedamola@gmail.com>
2024-09-25 16:42:53 +0100

    fix bug regarding map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,5 @@
 import "leaflet/dist/leaflet.css";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import { locationsCoordinates } from "./locationCoordinates";
 
@@ -13,7 +13,6 @@ const getRandomOffset = () => {
   const offset = 0.0001;
   return Math.random() * offset - offset / 2;
 };
-
 
 function FitMapToBounds({ coordinates }: { coordinates: Coordinate[] }): null {
   const map = useMap();
@@ -31,14 +30,6 @@ function FitMapToBounds({ coordinates }: { coordinates: Coordinate[] }): null {
 }
 
 function Map() {
-  const [locationCoordinates, setLocationCoordinates] = useState<Coordinate[]>(locationsCoordinates);
-
-  // Define the bounds that cover the Unilag perimeter as LatLngBoundsLiteral
-  /* const unilagBounds: LatLngBoundsLiteral = [
-    [6.9021,3.3934], // North-West corner
-    [6.515418, 3.405025], // South-East corner
-  ]; */
-
   return (
     <MapContainer
       center={[6.51788, 3.38672]} 
@@ -46,18 +37,15 @@ function Map() {
       minZoom={14} 
       style={{ height: "500px", width: "100%" }}
       scrollWheelZoom={true} 
-     
     >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
 
- 
-      <FitMapToBounds coordinates={locationCoordinates} />
+      <FitMapToBounds coordinates={locationsCoordinates} />
 
-
-      {locationCoordinates.map((location, index) => (
+      {locationsCoordinates.map((location, index) => (
         <Marker
           key={index}
           position={[
