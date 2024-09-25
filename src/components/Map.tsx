6525b610ac8140894b2e0928704b81605fbd0099
adedamola-toye <toye.adedamola@gmail.com>
2024-09-25 17:06:23 +0100

    Trying to solve map icon issue

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,7 +1,12 @@
 import "leaflet/dist/leaflet.css";
 import { useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import { Icon } from "leaflet"; // Import Icon
 import { locationsCoordinates } from "./locationCoordinates";
+
+// Import the marker icon images from Leaflet
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 interface Coordinate {
   location: string;
@@ -22,12 +27,22 @@ function FitMapToBounds({ coordinates }: { coordinates: Coordinate[] }): null {
       const bounds = coordinates.map(
         (coordinate) => [coordinate.latitude, coordinate.longitude] as [number, number]
       );
-      map.fitBounds(bounds); 
+      map.fitBounds(bounds);
     }
   }, [coordinates, map]);
 
-  return null; 
+  return null;
 }
+
+// Define a custom default icon for Leaflet markers
+const defaultIcon = new Icon({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41], // size of the icon
+  iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
+  popupAnchor: [1, -34], // point from which the popup should open relative to the iconAnchor
+  shadowSize: [41, 41] // size of the shadow
+});
 
 function Map() {
   return (
@@ -36,7 +51,7 @@ function Map() {
       zoom={15} 
       minZoom={14} 
       style={{ height: "500px", width: "100%" }}
-      scrollWheelZoom={true} 
+      scrollWheelZoom={true}
     >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -49,11 +64,12 @@ function Map() {
         <Marker
           key={index}
           position={[
-            location.latitude + getRandomOffset(), 
+            location.latitude + getRandomOffset(),
             location.longitude + getRandomOffset(),
           ]}
+          icon={defaultIcon} // Set the custom icon for each marker
         >
-          <Popup>{location.location}</Popup> 
+          <Popup>{location.location}</Popup>
         </Marker>
       ))}
     </MapContainer>
